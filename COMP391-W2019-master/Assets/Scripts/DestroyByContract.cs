eded9b38d5c129cf0894ee31034a7e944eb1e7e1
edwinscb <edwinsantiagocastrobuitrago@gmail.com>
2019-03-15 19:05:13 -0400

    .

---
+++ COMP391-W2019-master/Assets/Scripts/DestroyByContract.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContract : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject explosionPlayer;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boundary")
+        {
+            return;
+        }
+
+        if(other.tag == "Player")
+        {
+            // Trigger game over logic in here
+            GameObject temp = Instantiate(explosionPlayer, other.transform.position, other.transform.rotation);
+            Destroy(temp, 2.0f);
+            Destroy(other.gameObject);
+        }
+
+        // Debug.Log(other.gameObject.name);
+
+        // Instantiate asteroid explosion animation
+        Instantiate(explosion, this.transform.position, this.transform.rotation);
+
+        Destroy(other.gameObject);  // Laser
+        Destroy(this.gameObject);   // Asteroid
+    }
+}
